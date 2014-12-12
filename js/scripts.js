2c10ca37be14141204d2df30fe233a107fce2ad7
hani1980 <hani.mnshi@gmail.com>
2014-12-11 16:11:17 -0800

    commit 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,51 @@
-// Docs at http://simpleweatherjs.com
 $(document).ready(function() {
-  $.simpleWeather({
-    location: 'Austin, TX',
-    woeid: '',
-    unit: 'f',
-    success: function(weather) {
-      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
-      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
-      html += '<li class="currently">'+weather.currently+'</li>';
-      html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
   
-      $("#weather").html(html);
-    },
-    error: function(error) {
-      $("#weather").html('<p>'+error+'</p>');
-    }
-  });
-});
+  	//Get Zip Code//
+	$('button').click(function(){
+		
+		//remove class
+		
+		
+		//store the weather//
+		
+		var zip = $('input').val();
+  		console.log(zip);
+		
+		$.simpleWeather({
+	
+		// Minimum properties to set
+		zipcode: zip,  /*zipcode*/
+		unit: 'c',   /*farenheit*/
+		
+		// Method to display weather
+		success: function(weather) {
+			
+			// Concatenate HTML code & weather variables.
+			// Put into one big'ol collection
+			html = '<h2>'+weather.temp +'&deg;'+weather.units.temp+'</h2>';  //what yahoo is giving us//
+			html += '<ul><li>'+weather.city+', '+weather.region+'</li>';    //city//
+			html += '<ul><li> <img src="img/' + weather.code + '.png"></li>';
+			html += '<li class="currently">'+ weather.currently +'</li>';   //status-cloudy, sunny, rain. . .//
+			html += '<a href="'+weather.link+'">View Forecast &raquo;</a>';
+			$('body').addClass('bg' + weather.code);
+			html += '<p>'+'</p>';
+			
+			// Write collection to page with html() method
+			// Note: difference between html() method vs. html option 
+			$("#weather").html(html);
+			
+		
+			
+		},
+
+		// Method 
+		error: function(error) {
+			$("#weather").html('<p>'+error+'</p>');
+		}
+
+	});
+	// end getting Weather
+	
+});  
+  
+});	
